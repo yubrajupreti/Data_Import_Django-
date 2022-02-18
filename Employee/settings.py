e667efd736af862ab81e31f679690c05c2ee1e87
yubrajupreti <yubrajupreti2000@gmail.com>
2022-02-18 21:49:58 +0545

    build file change

--- Employee/settings.py
+++ Employee/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://employee-nepal.herokuapp.com']
 
 
 # Application definition
